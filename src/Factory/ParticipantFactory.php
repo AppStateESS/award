8c14e0b2c293907f951f9caa87638f03d8dff2a8
Matt McNaney <mcnaneym@appstate.edu>
2022-04-18 12:51:11 -0400

    Adds create function for a new participant.
    * Adds getByEmail function to retrieve participant be email address.

--- src/Factory/ParticipantFactory.php
+++ src/Factory/ParticipantFactory.php
@@ -13,8 +13,50 @@ declare(strict_types=1);
 
 namespace award\Factory;
 
-class ParticipantFactory
+use award\Resource\Participant;
+use phpws2\Database;
+
+class ParticipantFactory extends \award\AbstractFactory
 {
+
+    /**
+     * Creates a new participant using email and password parameter.
+     * Password is hashed by setPassword.
+     *
+     * @param string $email
+     * @param string $password
+     * @return Participant
+     */
+    public static function create(string $email, string $password)
+    {
+        $participant = new Participant;
+        $participant->setEmail($email)->setPassword($password)->createHash();
+        self::save($participant);
+        return $participant;
+    }
+
+    /**
+     * Returns a Participant object if exists, FALSE bool otherwise.
+     *
+     * @param string $email
+     * @return boolean|Participant
+     */
+    public static function getByEmail(string $email)
+    {
+        $db = Database::getDB();
+        $tbl = $db->addTable('award_participant');
+        $tbl->addFieldConditional('email', $email);
+        $result = $db->selectOneRow();
+
+        if (!$result) {
+            return false;
+        } else {
+            $participant = new Participant;
+            $result['id'] = (int) $result['id'];
+            $participant->setValues($result);
+            return $participant;
+        }
+    }
 
     public static function isSignedIn()
     {
