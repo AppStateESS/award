d9114b81b13aa68068897d904b26a87986542a63
Matt McNaney <mcnaneym@appstate.edu>
2022-04-18 12:49:33 -0400

    Adds comments to functions.
    * Adds createPost control function.

--- src/Controller/User/Participant.php
+++ src/Controller/User/Participant.php
@@ -14,17 +14,56 @@ declare(strict_types=1);
 
 namespace award\Controller\User;
 
+use Canopy\Request;
 use award\Controller\AbstractController;
 use award\View\ParticipantView;
+use award\Factory\ParticipantFactory;
+use award\Factory\EmailFactory;
 
 class Participant extends AbstractController
 {
 
+    /**
+     * Displays a form for a user to create a new participant. Submission is sent to
+     * self::createPost()
+     *
+     * @return string
+     */
     protected function createAccountHtml()
     {
         return ParticipantView::createAccount();
     }
 
+    /**
+     * Creates a new participant in the database and sends a confirmation email.
+     * If the participant already exists, a warning email is sent instead.
+     *
+     * @param Request $request
+     * @return type
+     */
+    protected function createPost(Request $request)
+    {
+        $email = $request->pullPostString('email');
+        $password = $request->pullPostString('password');
+        $participant = ParticipantFactory::getByEmail($email);
+        if ($participant) {
+            EmailFactory::createWarningOnExisting($participant);
+        } else {
+            $newParticipant = ParticipantFactory::create($email, $password);
+            EmailFactory::newParticipant($newParticipant);
+        }
+        return ['success' => true];
+    }
+
+//    protected function existsJson(Request $request)
+//    {
+//        return ['found' => true];
+//    }
+
+    /**
+     * Displays a form for a participant to sign in to the system.
+     * @return string
+     */
     protected function signinHtml()
     {
         return ParticipantView::signin();
