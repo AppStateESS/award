c24d8b95a1ed256c69223c2438ebe5217ef3468d
Matt McNaney <mcnaneym@appstate.edu>
2022-05-03 14:32:08 -0400

    Adds stub for CycleFactory.

---
+++ src/Factory/CycleFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Factory;
+
+use award\AbstractClass\AbstractFactory;
+
+class CycleFactory extends AbstractFactory
+{
+
+}
