df51e9df1e9d6e090b3755298f71b2a7650ff584
Matt McNaney <mcnaneym@appstate.edu>
2022-05-03 14:33:04 -0400

    Adds stub for DashboardView.

---
+++ src/View/DashboardView.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\View;
+
+use award\AbstractClass\AbstractView;
+
+class DashboardView extends AbstractView
+{
+
+    public static function admin()
+    {
+        $params['menu'] = self::menu('dashboard');
+        $params['upcoming'] = self::upcoming();
+        return self::getTemplate('Admin/Dashboard', $params, true);
+    }
+
+    private static function upcoming()
+    {
+        return CycleView::noteworthy();
+    }
+
+    private static function newAwardRequests()
+    {
+        AwardView::newAwardRequests();
+    }
+
+}
