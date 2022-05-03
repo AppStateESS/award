cd89feb7cd3d40c646b13d89747abb3f8518cf96
Matt McNaney <mcnaneym@appstate.edu>
2022-05-03 14:25:24 -0400

    Sets authorization types for local and appstate shib.

---
+++ config/system.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+/**
+ * Defines the type of authorization types used. AppstateShibboleth is
+ * obviously specific to this university. Local is expected to be in the zero slot
+ * and defines a participant who has entered their email and password locally in
+ * the system.
+ */
+define('AWARD_AUTH_TYPES', [0 => 'local', 1 => 'AppstateShibboleth']);
