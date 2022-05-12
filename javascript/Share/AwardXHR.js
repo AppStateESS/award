14f1950e97642ec6613e311fd08b20f495e2c651
Matt McNaney <mcnaneym@appstate.edu>
2022-05-12 13:35:16 -0400

    Removes getAwardList

--- javascript/Share/AwardXHR.js
+++ javascript/Share/AwardXHR.js
@@ -1,27 +1,7 @@
 import axios from 'axios'
+
 import 'regenerator-runtime'
 const headers = {'X-Requested-With': 'XMLHttpRequest'}
-
-/**
- * Queries the server for a listing of awards. The default role
- * is 'User' which will only show public awards.
- * @param {object} options
- * @returns
- */
-const getAwardList = async (options = {role: 'User'}) => {
-  const method = 'get'
-  const url = `./award/${options.role}/Award`
-  const data = {}
-  return await axios({
-    method,
-    url,
-    data,
-    timeout: 3000,
-    headers: {
-      'X-Requested-With': 'XMLHttpRequest',
-    },
-  })
-}
 
 const saveAward = async (award, role) => {
   const method = award.id > 0 ? 'put' : 'post'
@@ -34,10 +14,8 @@ const saveAward = async (award, role) => {
     url,
     data: award,
     timeout: 3000,
-    headers: {
-      'X-Requested-With': 'XMLHttpRequest',
-    },
+    headers,
   })
 }
 
-export {getAwardList, saveAward}
+export {saveAward}
