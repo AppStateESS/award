acc4ca442dd36852eed4bca196c534809aab0509
Matt McNaney <mcnaneym@appstate.edu>
2022-05-12 13:46:33 -0400

    Adds build function.

--- src/Factory/CycleFactory.php
+++ src/Factory/CycleFactory.php
@@ -14,8 +14,27 @@ declare(strict_types=1);
 namespace award\Factory;
 
 use award\AbstractClass\AbstractFactory;
+use award\Resource\Cycle;
+use phpws2\Database;
+use Canopy\Request;
 
 class CycleFactory extends AbstractFactory
 {
+
+    /**
+     * Initiates a Cycle Resource. If the $id is passed, a retrieval
+     * from the database is attempted.
+     * @param int $id
+     * @return award\Resource\Cycle
+     */
+    public static function build(int $id = 0): Cycle
+    {
+        $cycle = new Cycle;
+        if (!$id) {
+            return $cycle;
+        } else {
+            return self::load($cycle, $id);
+        }
+    }
 
 }
