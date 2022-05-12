2a23712e449be8898bbe7db15a3fc5c5c8d8dd53
Matt McNaney <mcnaneym@appstate.edu>
2022-05-12 13:36:23 -0400

    Adds getList and AbortController.
    getList assumes a signal parameter from an AbortController. This
    allows the list to destruct if interrupted.

--- javascript/Share/XHR.js
+++ javascript/Share/XHR.js
@@ -3,15 +3,30 @@ import 'regenerator-runtime'
 const headers = {'X-Requested-With': 'XMLHttpRequest'}
 
 const getItem = async (role, itemName, id) => {
+  const controller = new AbortController()
   const url = `award/${role}/${itemName}/${id}`
   try {
     const response = await axios.get(url, {
+      signal: controller.signal,
       headers,
     })
     return response
   } catch (error) {
+    controller.abort()
     return false
   }
 }
 
-export {getItem}
+const getList = async ({url, handleSuccess, handleError, signal}) => {
+  const params = {headers, signal}
+  return await axios
+    .get(url, params)
+    .then((response) => {
+      handleSuccess(response.data)
+    })
+    .catch((error) => {
+      handleError(error)
+    })
+}
+
+export {getItem, getList}
