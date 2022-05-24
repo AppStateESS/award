a75cfec1c8711ba78e464b69af19ba1614ca3596
Matt McNaney <mcnaneym@appstate.edu>
2022-05-24 14:46:07 -0400

    Adds adminList view.
    + Adds editForm view.

--- src/View/CycleView.php
+++ src/View/CycleView.php
@@ -13,10 +13,34 @@ declare(strict_types=1);
 
 namespace award\View;
 
+use award\Factory\AwardFactory;
 use award\AbstractClass\AbstractView;
+use award\Resource\Cycle;
 
 class CycleView extends AbstractView
 {
+
+    /**
+     * A cycle listing for administrators.
+     * @param int $awardId
+     */
+    public static function adminList(int $awardId = 0)
+    {
+        self::scriptView('CycleList', ['defaultAwardId' => $awardId]);
+
+        $tplValues['menu'] = self::menu('cycle');
+        return self::getTemplate('Admin/CycleList', $tplValues);
+    }
+
+    public static function editForm(Cycle $cycle): string
+    {
+        $award = AwardFactory::build($cycle->getAwardId());
+        $js['defaultCycle'] = $cycle->getValues();
+        $js['awardTitle'] = $award->getTitle();
+        $tplvars['menu'] = self::menu('award');
+        $tplvars['script'] = self::scriptView('CycleForm', $js);
+        return self::getTemplate('Admin/AdminForm', $tplvars);
+    }
 
     /**
      * Returns a listing of cycles that need attention (vote approval, nomination ending)
