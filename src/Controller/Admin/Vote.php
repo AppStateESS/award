872bb1f6c67a8eb6332939196c413d7f887bff35
Matt McNaney <mcnaneym@appstate.edu>
2022-05-24 15:06:36 -0400

    Adds admin controller for vote commands.

---
+++ src/Controller/Admin/Vote.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Controller\Admin;
+
+use award\AbstractClass\AbstractController;
+use award\Factory\VoteFactory;
+use Canopy\Request;
+
+class Vote extends AbstractController
+{
+
+    public function typesJson()
+    {
+        return VoteFactory::getTypeList();
+    }
+
+}
