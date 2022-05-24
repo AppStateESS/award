95043b10bc614f445636a58fb3bcfe4c328cf360
Matt McNaney <mcnaneym@appstate.edu>
2022-05-24 14:38:47 -0400

    Adds deleteItem function.
    + Changes getItem to return the promise response.
    + Adds saveResource function.

--- javascript/Share/XHR.js
+++ javascript/Share/XHR.js
@@ -2,23 +2,29 @@ import axios from 'axios'
 import 'regenerator-runtime'
 const headers = {'X-Requested-With': 'XMLHttpRequest'}
 
+const deleteItem = async (role, itemName, id) => {
+  const controller = new AbortController()
+  const url = `award/${role}/${itemName}/${id}`
+  return await axios.delete(url, {
+    signal: controller.signal,
+    headers,
+  })
+}
+
 const getItem = async (role, itemName, id) => {
   const controller = new AbortController()
   const url = `award/${role}/${itemName}/${id}`
-  try {
-    const response = await axios.get(url, {
-      signal: controller.signal,
-      headers,
-    })
-    return response
-  } catch (error) {
-    controller.abort()
-    return false
-  }
+  return await axios.get(url, {
+    signal: controller.signal,
+    headers,
+  })
 }
 
 const getList = async ({url, handleSuccess, handleError, signal}) => {
   const params = {headers, signal}
+  if (handleError === undefined) {
+    handleError = (e) => console.error(e)
+  }
   return await axios
     .get(url, params)
     .then((response) => {
@@ -29,4 +35,34 @@ const getList = async ({url, handleSuccess, handleError, signal}) => {
     })
 }
 
-export {getItem, getList}
+const saveResource = async ({
+  resource,
+  role,
+  resourceName,
+  success,
+  failure,
+}) => {
+  if (failure === undefined) {
+    failure = (e) => console.error(e)
+  }
+  const method = resource.id > 0 ? 'put' : 'post'
+  let url = `./award/${role}/${resourceName}`
+  if (resource.id > 0) {
+    url += '/' + resource.id
+  }
+  return await axios({
+    method,
+    url,
+    data: resource,
+    timeout: 3000,
+    headers,
+  })
+    .then((response) => {
+      success(response.data)
+    })
+    .catch((error) => {
+      failure(error)
+    })
+}
+
+export {getItem, getList, saveResource, deleteItem}
