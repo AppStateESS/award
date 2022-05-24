38a1f3c8bbcfd559223f5a6ea597c601ac5bbb9f
Matt McNaney <mcnaneym@appstate.edu>
2022-05-24 15:05:52 -0400

    Adds stub for abstract vote type for later.

---
+++ src/AbstractClass/AbstractVoteType.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\AbstractClass;
+
+class AbstractVoteType
+{
+//put your code here
+}
