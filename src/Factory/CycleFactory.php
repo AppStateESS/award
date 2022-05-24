e4c64bf4a071e852395b5c93024165dd0b5b9ff0
Matt McNaney <mcnaneym@appstate.edu>
2022-05-24 14:42:50 -0400

    Adds the list() function.
    + Adds a post() function.

--- src/Factory/CycleFactory.php
+++ src/Factory/CycleFactory.php
@@ -30,11 +30,44 @@ class CycleFactory extends AbstractFactory
     public static function build(int $id = 0): Cycle
     {
         $cycle = new Cycle;
+
         if (!$id) {
             return $cycle;
         } else {
             return self::load($cycle, $id);
         }
+    }
+
+    public static function list(array $options = [])
+    {
+        $db = parent::getDB();
+        $tbl = $db->addTable('award_cycle');
+        if (!empty($options['awardId'])) {
+            $tbl->addFieldConditional('awardId', (int) $options['awardId']);
+        }
+
+        $tbl->addOrderBy('startDate', 'desc');
+
+        return $db->select();
+    }
+
+    /**
+     * Copies the post information to cycle object.
+     * Does not set currentActive or voteAllowed. Both default to false.
+     * @param Request $request
+     */
+    public static function post(Request $request)
+    {
+        $cycle = self::build();
+
+        $cycle->setAwardId($request->pullPostInteger('awardId'));
+        $cycle->setAwardMonth($request->pullPostInteger('awardMonth'));
+        $cycle->setAwardYear($request->pullPostInteger('awardYear'));
+        $cycle->setEndDate($request->pullPostInteger('endDate'));
+        $cycle->setStartDate($request->pullPostInteger('startDate'));
+        $cycle->setTerm($request->pullPostString('term'));
+        $cycle->setVoteType($request->pullPostString('voteType'));
+        return $cycle;
     }
 
 }
