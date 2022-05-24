d7ee05726954348d9bd785160c8892faf455d1b1
Matt McNaney <mcnaneym@appstate.edu>
2022-05-24 15:05:20 -0400

    Adds stub for overlay modal.

---
+++ javascript/Share/Overlay.js
@@ -0,0 +1,10 @@
+'use strict'
+import React, {useState, useEffect} from 'react'
+import PropTypes from 'prop-types'
+
+const Overlay = () => {
+  return <div>Overlay is ready for content.</div>
+}
+
+Overlay.propTypes = {}
+export default Overlay
