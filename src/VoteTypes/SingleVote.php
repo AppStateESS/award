d57bd4e93800cd1dd0164a8162b29e9d8755bd55
Matt McNaney <mcnaneym@appstate.edu>
2022-05-24 15:07:08 -0400

    Adds stub for the single vote type.

---
+++ src/VoteTypes/SingleVote.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ *
+ * Allows each to vote for a single nomination.
+ */
+
+namespace award\VoteTypes;
+
+use award\AbstractClass\AbstractVoteType;
+
+class SingleVote extends AbstractVoteType
+{
+//put your code here
+}
