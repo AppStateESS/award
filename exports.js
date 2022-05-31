56bffe9c3d91109e16ec404a97dda4a6ccee5e69
Matt McNaney <mcnaneym@appstate.edu>
2022-05-31 11:57:05 -0400

    Switches to typescript

--- exports.js
+++ exports.js
@@ -5,7 +5,7 @@ exports.APP_DIR = exports.path.resolve(__dirname, 'javascript')
 exports.entry = {
   SignUpForm: exports.APP_DIR + '/SignUpForm/index.jsx',
   AwardList: exports.APP_DIR + '/Admin/AwardList/index.tsx',
-  AwardForm: exports.APP_DIR + '/Admin/AwardForm/index.jsx',
-  CycleForm: exports.APP_DIR + '/Admin/CycleForm/index.jsx',
-  CycleList: exports.APP_DIR + '/Admin/CycleList/index.jsx',
+  AwardForm: exports.APP_DIR + '/Admin/AwardForm/index.tsx',
+  CycleForm: exports.APP_DIR + '/Admin/CycleForm/index.tsx',
+  CycleList: exports.APP_DIR + '/Admin/CycleList/index.tsx',
 }
