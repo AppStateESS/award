e19be8aa64b132d5bd1b45920c6dded99cd51747
Matt McNaney <mcnaneym@appstate.edu>
2022-06-06 14:56:45 -0400

    Adds hideFromProfile param, getter, and setter.

--- src/Resource/CycleWinner.php
+++ src/Resource/CycleWinner.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 /**
  * MIT License
@@ -11,11 +12,13 @@ declare(strict_types=1);
  */
 
 namespace award\Resource;
+
 /**
- * @table cyclewinner 
+ * @table cyclewinner
  */
 class CycleWinner extends \award\AbstractResource
 {
+
     /**
      * @var int
      */
@@ -42,9 +45,17 @@ class CycleWinner extends \award\AbstractResource
     private int $nominationId;
 
     /**
+     * If true, the award will not be shown on the winner's profile or
+     * exported.
+     *
+     * @var bool
+     */
+    private bool $hideFromProfile = false;
+
+    /**
      * @return int
      */
-    public function getAwardId() : int
+    public function getAwardId(): int
     {
         return $this->awardId;
     }
@@ -52,7 +63,7 @@ class CycleWinner extends \award\AbstractResource
     /**
      * @return int
      */
-    public function getCycleId() : int
+    public function getCycleId(): int
     {
         return $this->cycleId;
     }
@@ -60,15 +71,20 @@ class CycleWinner extends \award\AbstractResource
     /**
      * @return string
      */
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return $this->description;
+    }
+
+    public function getHideFromProfile(): bool
+    {
+        return $this->hideFromProfile;
     }
 
     /**
      * @return string
      */
-    public function getImage() : string
+    public function getImage(): string
     {
         return $this->image;
     }
@@ -76,7 +92,7 @@ class CycleWinner extends \award\AbstractResource
     /**
      * @return int
      */
-    public function getNominationId() : int
+    public function getNominationId(): int
     {
         return $this->nominationId;
     }
@@ -84,7 +100,7 @@ class CycleWinner extends \award\AbstractResource
     /**
      * @param int $awardId
      */
-    public function setAwardId(int $awardId) : self
+    public function setAwardId(int $awardId): self
     {
         $this->awardId = $awardId;
         return $this;
@@ -93,7 +109,7 @@ class CycleWinner extends \award\AbstractResource
     /**
      * @param int $cycleId
      */
-    public function setCycleId(int $cycleId) : self
+    public function setCycleId(int $cycleId): self
     {
         $this->cycleId = $cycleId;
         return $this;
@@ -102,16 +118,22 @@ class CycleWinner extends \award\AbstractResource
     /**
      * @param string $description
      */
-    public function setDescription(string $description) : self
+    public function setDescription(string $description): self
     {
         $this->description = $description;
+        return $this;
+    }
+
+    public function setHideFromProfile(bool $hideFromProfile): self
+    {
+        $this->hideFromProfile = $hideFromProfile;
         return $this;
     }
 
     /**
      * @param string $image
      */
-    public function setImage(string $image) : self
+    public function setImage(string $image): self
     {
         $this->image = $image;
         return $this;
@@ -120,7 +142,7 @@ class CycleWinner extends \award\AbstractResource
     /**
      * @param int $nominationId
      */
-    public function setNominationId(int $nominationId) : self
+    public function setNominationId(int $nominationId): self
     {
         $this->nominationId = $nominationId;
         return $this;
