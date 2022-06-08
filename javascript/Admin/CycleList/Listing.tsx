1d26393313a354217a6d8b1156f936a420bddadc
Matt McNaney <mcnaneym@appstate.edu>
2022-06-08 11:21:00 -0400

    Adds delete functionality.

--- javascript/Admin/CycleList/Listing.tsx
+++ javascript/Admin/CycleList/Listing.tsx
@@ -1,38 +1,50 @@
 'use strict'
-import React, {Fragment} from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
-//import {deleteItem} from '../../Share/XHR'
+import {deleteItem} from '../../Share/XHR'
 import {CycleResource} from '../../ResourceTypes'
+import Modal from '../../Share/Modal'
+import DeletePrompt from './DeletePrompt'
 
-const Listing = ({cycleListing}: {cycleListing: CycleResource[]}) => {
-  const select = (id: number) => {
+const Listing = ({
+  cycleListing,
+  reload,
+}: {
+  cycleListing: CycleResource[]
+  reload: () => void
+}) => {
+  const [deleteModal, setDeleteModal] = useState(false)
+  const [currentCycle, setCurrentCycle] = useState<CycleResource>()
+  const [deleteConfirm, setDeleteConfirm] = useState(false)
+
+  const select = (cycle: CycleResource) => {
+    const [current, setCurrent] = useState('option')
     return (
-      <Fragment>
-        <select
-          defaultValue="option"
-          onChange={(e) => {
-            switch (e.target.value) {
-              case '1':
-                location.href = `./award/Admin/Cycle/${id}`
-                break
-              case '2':
-                location.href = `./award/Admin/Cycle/${id}/edit`
-                break
-              case '3':
-                if (confirm('Are you sure you want to delete this cycle?')) {
-                  console.log('DELETED!')
-                }
-                break
-            }
-          }}>
-          <option disabled value="option">
-            -- Options --
-          </option>
-          <option value="1">View</option>
-          <option value="2">Edit</option>
-          <option value="3">Delete</option>
-        </select>
-      </Fragment>
+      <select
+        value={current}
+        defaultValue="option"
+        onChange={(e) => {
+          switch (e.target.value) {
+            case '1':
+              location.href = `./award/Admin/Cycle/${cycle.id}`
+              break
+            case '2':
+              location.href = `./award/Admin/Cycle/${cycle.id}/edit`
+              break
+            case '3':
+              setCurrent('option')
+              setCurrentCycle(cycle)
+              setDeleteModal(true)
+              break
+          }
+        }}>
+        <option disabled value="option">
+          -- Options --
+        </option>
+        <option value="1">View</option>
+        <option value="2">Edit</option>
+        <option value="3">Delete</option>
+      </select>
     )
   }
 
@@ -43,7 +55,7 @@ const Listing = ({cycleListing}: {cycleListing: CycleResource[]}) => {
   const rows = cycleListing.map((value) => {
     return (
       <tr key={`cycle-${value.id}`}>
-        <td>{select(value.id)}</td>
+        <td>{select(value)}</td>
         <td>{value.awardMonth}</td>
         <td>{value.awardYear}</td>
         <td>{value.term}</td>
@@ -52,8 +64,47 @@ const Listing = ({cycleListing}: {cycleListing: CycleResource[]}) => {
       </tr>
     )
   })
+
+  const deleteCycle = () => {
+    setDeleteConfirm(false)
+    setDeleteModal(false)
+    if (currentCycle) {
+      deleteItem('Admin', 'Cycle', currentCycle.id).then(() => {
+        reload()
+      })
+    }
+  }
+
+  let deleteButton
+  if (deleteConfirm) {
+    deleteButton = (
+      <button className="btn btn-danger" onClick={deleteCycle}>
+        Are you sure?
+      </button>
+    )
+  } else {
+    deleteButton = (
+      <button
+        className="btn btn-danger"
+        onClick={() => {
+          setDeleteConfirm(true)
+        }}>
+        Delete
+      </button>
+    )
+  }
   return (
     <div>
+      <Modal
+        title="Delete cycle"
+        show={deleteModal}
+        button={deleteButton}
+        close={() => {
+          setDeleteModal(false)
+          setDeleteConfirm(false)
+        }}>
+        <DeletePrompt cycle={currentCycle} />
+      </Modal>
       <table className="table table-striped">
         <tbody>{rows}</tbody>
       </table>
