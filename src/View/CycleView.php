e5b75f3729b63bcf32bed0dcc8b2a10e9306a458
Matt McNaney <mcnaneym@appstate.edu>
2022-06-09 13:26:34 -0400

    Adds currentCycleWarning function.
    + Fixes menu selection.

--- src/View/CycleView.php
+++ src/View/CycleView.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 namespace award\View;
 
 use award\Factory\AwardFactory;
+use award\Factory\CycleFactory;
 use award\AbstractClass\AbstractView;
 use award\Resource\Cycle;
 
@@ -26,10 +27,18 @@ class CycleView extends AbstractView
      */
     public static function adminList(int $awardId = 0)
     {
-
         $tplValues['menu'] = self::menu('cycle');
         $tplValues['script'] = self::scriptView('CycleList', ['defaultAwardId' => $awardId]);
         return self::getTemplate('Admin/AdminForm', $tplValues);
+    }
+
+    public static function currentCycleWarning(\award\Resource\Award $award)
+    {
+        $cycle = CycleFactory::build($award->getCycleId());
+        $tpl = $cycle->getValues();
+        $tpl['menu'] = self::menu('cycle');
+
+        return self::getTemplate('Admin/CurrentCycleWarning', $tpl);
     }
 
     public static function editForm(Cycle $cycle): string
@@ -37,7 +46,7 @@ class CycleView extends AbstractView
         $award = AwardFactory::build($cycle->getAwardId());
         $js['defaultCycle'] = $cycle->getValues();
         $js['awardTitle'] = $award->getTitle();
-        $tplvars['menu'] = self::menu('award');
+        $tplvars['menu'] = self::menu('cycle');
         $tplvars['script'] = self::scriptView('CycleForm', $js);
         return self::getTemplate('Admin/AdminForm', $tplvars);
     }
