9185e96124592e70393688a5ea02bd4e72156c82
Matt McNaney <mcnaneym@appstate.edu>
2022-07-11 12:49:41 -0400

    Adds several new defines.
    + Adds invitation status defines.
    + Adds invitation type defines.
    + Changes default vote type to SingleVote.

--- config/system.php
+++ config/system.php
@@ -10,10 +10,22 @@
  * @license https://opensource.org/licenses/MIT
  */
 /**
+ * System defines. Do not alter these settings!
+ */
+/**
  * Defines the type of authorization types used. AppstateShibboleth is
  * obviously specific to this university. Local is expected to be in the zero slot
  * and defines a participant who has entered their email and password locally in
  * the system.
  */
 define('AWARD_AUTH_TYPES', [0 => 'local', 1 => 'AppstateShibboleth']);
-define('AWARD_DEFAULT_VOTE_TYPE', 'choice');
+define('AWARD_DEFAULT_VOTE_TYPE', 'SingleVote');
+
+define('AWARD_INVITATION_WAITING', 0);
+define('AWARD_INVITATION_CONFIRMED', 1);
+define('AWARD_INVITATION_REFUSED', 2);
+
+define('AWARD_INVITE_TYPE_NEW', 0);
+define('AWARD_INVITE_TYPE_JUDGE', 1);
+define('AWARD_INVITE_TYPE_REFERENCE', 2);
+define('AWARD_INVITE_TYPE_NOMINATED', 3);
