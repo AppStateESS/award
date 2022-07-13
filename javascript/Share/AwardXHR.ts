0883cf8f69ff38c58a4faa3c0a39282951ba62d9
Matt McNaney <mcnaneym@appstate.edu>
2022-07-13 13:20:09 -0400

    Adds getSelectList function to return an award selection list.

--- javascript/Share/AwardXHR.ts
+++ javascript/Share/AwardXHR.ts
@@ -14,6 +14,14 @@ const activate = async (awardId: number, active: boolean) => {
   })
 }
 
+/**
+ * Expects an array response containing objects of just the id and title.
+ * @returns Promise
+ */
+const getSelectList = async () => {
+  return axios.get('award/Admin/Award/?asSelect=true', {headers})
+}
+
 const saveAward = async (award: AwardResource, role: string) => {
   const method = award.id > 0 ? 'put' : 'post'
   let url = `./award/${role}/Award`
@@ -33,4 +41,4 @@ const getHasCycles = async (awardId: number) => {
   return axios.get(`award/Admin/Award/${awardId}/hasCycles`, {headers})
 }
 
-export {activate, getHasCycles, saveAward}
+export {activate, getHasCycles, saveAward, getSelectList}
