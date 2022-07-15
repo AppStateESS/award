ee8c1077ff6cf9e04635b6109bdb9e0d1041b2e5
Matt McNaney <mcnaneym@appstate.edu>
2022-07-15 14:06:02 -0400

    Adds Setting script.

--- exports.js
+++ exports.js
@@ -3,11 +3,12 @@ exports.path = require('path')
 exports.APP_DIR = exports.path.resolve(__dirname, 'javascript')
 
 exports.entry = {
-  SignUpForm: exports.APP_DIR + '/SignUpForm/index.jsx',
   AwardList: exports.APP_DIR + '/Admin/AwardList/index.tsx',
   AwardForm: exports.APP_DIR + '/Admin/AwardForm/index.tsx',
   CycleForm: exports.APP_DIR + '/Admin/CycleForm/index.tsx',
   CycleList: exports.APP_DIR + '/Admin/CycleList/index.tsx',
-  ParticipantList: exports.APP_DIR + '/Admin/ParticipantList/index.tsx',
   InvitationList: exports.APP_DIR + '/Admin/InvitationList/index.tsx',
+  ParticipantList: exports.APP_DIR + '/Admin/ParticipantList/index.tsx',
+  Setting: exports.APP_DIR + '/Setting/index.tsx',
+  SignUpForm: exports.APP_DIR + '/SignUpForm/index.jsx',
 }
