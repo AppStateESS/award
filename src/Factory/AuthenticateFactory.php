a17e3ed3836327ab06f7af9d2e10ce477a515a1b
Matt McNaney <mcnaneym@appstate.edu>
2022-07-15 14:39:48 -0400

    Adds new functions to help with authentication management.

--- src/Factory/AuthenticateFactory.php
+++ src/Factory/AuthenticateFactory.php
@@ -13,9 +13,51 @@ namespace award\Factory;
 class AuthenticateFactory
 {
 
-    public function getAuthtypeList()
+    const authDirectory = PHPWS_SOURCE_DIR . 'mod/award/src/Authtypes/';
+
+    /**
+     * Returns an array of authentication files in the award directory.
+     * @return boolean | array
+     */
+    public static function getAuthtypeList()
     {
-        var_dump();
+        $filtered = self::getFileList();
+        if ($filtered === false) {
+            return false;
+        }
+        return self::authenticationInfo($filtered);
+    }
+
+    private static function authenticationInfo(array $filelist)
+    {
+        $authenticators = SettingFactory::getEnabledAuthenticators();
+        $info = [];
+        foreach ($filelist as $file) {
+            $className = 'award\\Authtypes\\' . str_replace('.php', '', $file);
+            require_once self::authDirectory . $file;
+            $obj = new $className;
+            $info[] = ['filename' => $file, 'title' => $obj->getTitle(), 'enabled' => in_array($file, $authenticators)];
+        }
+        return $info;
+    }
+
+    private static function getFileList()
+    {
+        $authTypeFiles = scandir(self::authDirectory);
+        if (!$authTypeFiles) {
+            return false;
+        }
+        $filtered = [];
+        foreach ($authTypeFiles as $filename) {
+            if (preg_match('/\.php$/', $filename)) {
+                $filtered[] = $filename;
+            }
+        }
+        if (empty($filtered)) {
+            return false;
+        } else {
+            return $filtered;
+        }
     }
 
 }
