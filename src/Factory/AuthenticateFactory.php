6f97b3fb029f076ab62cab2184f3f557e4a6b373
Matt McNaney <mcnaneym@appstate.edu>
2022-07-15 07:46:58 -0400

    Renames Authenticate.

---
+++ src/Factory/AuthenticateFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This is written strictly for Shibboleth. Should authentication change in some manner it will
+ * need to be reworked.
+ * @deprecated
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Factory;
+
+class AuthenticateFactory
+{
+
+    public function getAuthtypeList()
+    {
+        var_dump();
+    }
+
+}
