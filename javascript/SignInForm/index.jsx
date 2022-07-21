22192ed5153286320bf079009d953d21f078a291
Matt McNaney <mcnaneym@appstate.edu>
2022-07-21 13:21:05 -0400

    Moves sign in form to modal so alternate signins can be used.

--- javascript/SignInForm/index.jsx
+++ javascript/SignInForm/index.jsx
@@ -2,8 +2,10 @@
 import React, {useState, useEffect} from 'react'
 import {signInPost} from '../Share/ParticipantXHR'
 import {createRoot} from 'react-dom/client'
+import Modal from '../Share/Modal'
 
 const SignInForm = () => {
+  const [showModal, setShowModal] = useState(false)
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [emailError, setEmailError] = useState(false)
@@ -56,61 +58,77 @@ const SignInForm = () => {
 
   return (
     <div>
-      {signInResult.length > 0 ? (
-        <div className="alert alert-danger">{signInResult}</div>
-      ) : null}
-      <div className="form-group row">
-        <div className="col-4 col-form-label">Email address</div>
-        <div className="col">
-          <input
-            type="text"
-            name="email"
-            value={email}
-            onBlur={checkEmail}
-            className="form-control"
-            onChange={(e) => {
-              setEmail(e.target.value)
-            }}
-          />
-          {emailError ? (
-            <span className="badge badge-danger">
-              Please enter your email address
-            </span>
-          ) : null}
+      <button
+        className="btn btn-success btn-block"
+        onClick={() => {
+          setShowModal(true)
+        }}>
+        Sign in locally
+      </button>
+      <Modal
+        show={showModal}
+        title="Sign in locally"
+        close={() => setShowModal(false)}>
+        <div className="container">
+          <div className="form-group row">
+            {signInResult.length > 0 ? (
+              <div className="alert alert-danger">{signInResult}</div>
+            ) : null}
+            <div className="col-4 col-form-label">Email address</div>
+            <div className="col">
+              <input
+                type="text"
+                name="email"
+                value={email}
+                onBlur={checkEmail}
+                className="form-control"
+                onChange={(e) => {
+                  setEmail(e.target.value)
+                }}
+              />
+              {emailError ? (
+                <span className="badge badge-danger">
+                  Please enter your email address
+                </span>
+              ) : null}
+            </div>
+          </div>
+          <div className="form-group row">
+            <div className="col-4 col-form-label">Password</div>
+            <div className="col">
+              <input
+                type="password"
+                name="password"
+                onBlur={checkPassword}
+                value={password}
+                className="form-control"
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+                onKeyDown={checkEnter}
+              />
+              {passwordError ? (
+                <span className="badge badge-danger">
+                  Please enter your password
+                </span>
+              ) : null}
+              <a
+                className="small"
+                href="./award/User/Participant/forgotPassword">
+                I forgot my password
+              </a>
+            </div>
+          </div>
+          <div className="text-center">
+            <button
+              className="btn btn-primary"
+              onClick={signIn}
+              disabled={emailError || passwordError}>
+              Sign In
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="form-group row">
-        <div className="col-4 col-form-label">Password</div>
-        <div className="col">
-          <input
-            type="password"
-            name="password"
-            onBlur={checkPassword}
-            value={password}
-            className="form-control"
-            onChange={(e) => {
-              setPassword(e.target.value)
-            }}
-            onKeyDown={checkEnter}
-          />
-          {passwordError ? (
-            <span className="badge badge-danger">
-              Please enter your password
-            </span>
-          ) : null}
-          <a className="small" href="./award/User/Participant/forgotPassword">
-            I forgot my password
-          </a>
-        </div>
-      </div>
-      <div className="text-center">
-        <button
-          className="btn btn-primary"
-          onClick={signIn}
-          disabled={emailError || passwordError}>
-          Sign In
-        </button>
-      </div>
+      </Modal>
     </div>
   )
 }
