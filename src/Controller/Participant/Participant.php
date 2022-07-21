c789d9868a4973df344ef81fc91d269f14cc20c4
Matt McNaney <mcnaneym@appstate.edu>
2022-07-21 13:37:11 -0400

    Adds new controller for Participant functionality.
    Just a stub for now.

---
+++ src/Controller/Participant/Participant.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * This class is the controller for authenticated Participants.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Controller\Participant;
+
+use Canopy\Request;
+use award\AbstractClass\AbstractController;
+use award\View\ParticipantView;
+use award\Factory\ParticipantFactory;
+use award\Factory\EmailFactory;
+use award\Factory\Authenticate;
+
+class Participant extends AbstractController
+{
+
+    protected function dashboardHtml()
+    {
+        return 'Participant dashboard here';
+    }
+
+}
