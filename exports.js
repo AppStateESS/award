a90b978b1eefb6ae1a0deed762ee5b75c1509012
Matt McNaney <mcnaneym@appstate.edu>
2022-07-21 13:20:05 -0400

    Adds ForgotPassword and SignInForm export scripts.

--- exports.js
+++ exports.js
@@ -7,8 +7,10 @@ exports.entry = {
   AwardForm: exports.APP_DIR + '/Admin/AwardForm/index.tsx',
   CycleForm: exports.APP_DIR + '/Admin/CycleForm/index.tsx',
   CycleList: exports.APP_DIR + '/Admin/CycleList/index.tsx',
+  ForgotPassword: exports.APP_DIR + '/ForgotPassword/index.tsx',
   InvitationList: exports.APP_DIR + '/Admin/InvitationList/index.tsx',
   ParticipantList: exports.APP_DIR + '/Admin/ParticipantList/index.tsx',
   Setting: exports.APP_DIR + '/Setting/index.tsx',
+  SignInForm: exports.APP_DIR + '/SignInForm/index.jsx',
   SignUpForm: exports.APP_DIR + '/SignUpForm/index.jsx',
 }
