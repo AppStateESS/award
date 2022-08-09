2cf3aa12d6e045c5318f71fcbfc87c594ee00115
Matt McNaney <mcnaneym@appstate.edu>
2022-08-09 10:04:43 -0400

    Shifts script into directories.

--- exports.js
+++ exports.js
@@ -2,17 +2,22 @@
 exports.path = require('path')
 exports.APP_DIR = exports.path.resolve(__dirname, 'javascript')
 
-exports.entry = {
+const adminScripts = {
   AwardList: exports.APP_DIR + '/Admin/AwardList/index.tsx',
   AwardForm: exports.APP_DIR + '/Admin/AwardForm/index.tsx',
   CycleForm: exports.APP_DIR + '/Admin/CycleForm/index.tsx',
   CycleList: exports.APP_DIR + '/Admin/CycleList/index.tsx',
-  ForgotPassword: exports.APP_DIR + '/ForgotPassword/index.tsx',
   InvitationList: exports.APP_DIR + '/Admin/InvitationList/index.tsx',
   Judges: exports.APP_DIR + '/Admin/Judges/index.tsx',
   ParticipantList: exports.APP_DIR + '/Admin/ParticipantList/index.tsx',
-  ResetPassword: exports.APP_DIR + '/ResetPassword/index.tsx',
-  Setting: exports.APP_DIR + '/Setting/index.tsx',
-  SignInForm: exports.APP_DIR + '/SignInForm/index.jsx',
-  SignUpForm: exports.APP_DIR + '/SignUpForm/index.jsx',
+  Setting: exports.APP_DIR + '/Admin/Setting/index.tsx',
 }
+
+const userScripts = {
+  ForgotPassword: exports.APP_DIR + '/User/ForgotPassword/index.tsx',
+  ResetPassword: exports.APP_DIR + '/User/ResetPassword/index.tsx',
+  SignInForm: exports.APP_DIR + '/User/SignInForm/index.tsx',
+  SignUpForm: exports.APP_DIR + '/User/SignUpForm/index.tsx',
+}
+
+exports.entry = {...adminScripts, ...userScripts}
