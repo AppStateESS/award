2f8498fb015837893347265aabf350d493aab886
Matt McNaney <mcnaneym@appstate.edu>
2022-08-19 15:10:28 -0400

    Adds first and last name inputs on sign up.

--- javascript/User/SignUpForm/index.tsx
+++ javascript/User/SignUpForm/index.tsx
@@ -7,8 +7,12 @@ declare const defaultEmail: string
 
 const SignUpForm = () => {
   const [email, setEmail] = useState(defaultEmail)
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
   const [password1, setPassword1] = useState('')
   const [password2, setPassword2] = useState('')
+  const [firstNameError, setFirstNameError] = useState(false)
+  const [lastNameError, setLastNameError] = useState(false)
   const [emailError, setEmailError] = useState(false)
   const [passwordError, setPasswordError] = useState(false)
   const [showPassword, setShowPassword] = useState(false)
@@ -22,10 +26,16 @@ const SignUpForm = () => {
   }, [])
 
   const checkAndSave = () => {
-    if (emailError || passwordError || email.length === 0) {
+    if (
+      emailError ||
+      passwordError ||
+      firstName.length === 0 ||
+      lastName.length === 0 ||
+      email.length === 0
+    ) {
       return
     }
-    saveNewParticipant(email, password1)
+    saveNewParticipant({email, password: password1, firstName, lastName})
       .then((response) => {
         if (response.data.success === true) {
           location.href = './award/User/Participant/emailSent'
@@ -64,6 +74,49 @@ const SignUpForm = () => {
   return (
     <div>
       <div className="form-group row">
+        <label className="col-sm-4 col-form-label" htmlFor="firstName">
+          First/Chosen name
+        </label>
+        <div className="col-sm-8">
+          <input
+            type="text"
+            tabIndex={0}
+            ref={inputVar}
+            className="form-control"
+            name="firstName"
+            value={firstName}
+            onBlur={() => setFirstNameError(firstName.length === 0)}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          {firstNameError ? (
+            <span className="badge badge-danger">
+              Please type your first/chosen name.
+            </span>
+          ) : null}
+        </div>
+      </div>
+      <div className="form-group row">
+        <label className="col-sm-4 col-form-label" htmlFor="lastName">
+          Last name
+        </label>
+        <div className="col-sm-8">
+          <input
+            type="text"
+            tabIndex={0}
+            className="form-control"
+            name="lastName"
+            onBlur={() => setLastNameError(lastName.length === 0)}
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          {lastNameError ? (
+            <span className="badge badge-danger">
+              Please type your last name.
+            </span>
+          ) : null}
+        </div>
+      </div>
+      <div className="form-group row">
         <label className="col-sm-4 col-form-label" htmlFor="email">
           Your email address
         </label>
@@ -72,7 +125,6 @@ const SignUpForm = () => {
             type="text"
             tabIndex={0}
             className="form-control"
-            ref={inputVar}
             name="email"
             value={email}
             onBlur={matchEmail}
@@ -93,7 +145,7 @@ const SignUpForm = () => {
           <div className="input-group mb-3">
             <input
               type={showPassword ? 'text' : 'password'}
-              tabIndex={1}
+              tabIndex={0}
               className="form-control"
               name="password1"
               onBlur={checkPassword}
@@ -101,10 +153,7 @@ const SignUpForm = () => {
               onChange={(e) => setPassword1(e.target.value)}
             />
             <div className="input-group-append">
-              <button
-                tabIndex={3}
-                className="btn btn-outline-dark"
-                onClick={togglePassword}>
+              <button className="btn btn-outline-dark" onClick={togglePassword}>
                 <i className="fas fa-eye"></i>
               </button>
             </div>
@@ -124,7 +173,7 @@ const SignUpForm = () => {
         <div className="col-sm-8">
           <input
             type={showPassword ? 'text' : 'password'}
-            tabIndex={2}
+            tabIndex={0}
             className="form-control"
             name="password2"
             onBlur={checkPassword}
@@ -134,6 +183,7 @@ const SignUpForm = () => {
         </div>
       </div>
       <button
+        tabIndex={0}
         className="btn btn-success btn-block"
         onClick={checkAndSave}
         disabled={disableSave}>
