7c21c4f48d923ff9d505c74aa6de05da5f499e24
Matt McNaney <mcnaneym@appstate.edu>
2022-08-23 12:45:25 -0400

    New factory for References. Currently stub.

---
+++ src/Factory/ReferenceFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Factory;
+
+use award\Resource\Reference;
+use award\AbstractClass\AbstractFactory;
+use award\View\EmailView;
+use phpws2\Database;
+
+class ReferenceFactory extends AbstractFactory
+{
+//put your code here
+}
