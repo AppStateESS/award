18dd6db941da5416c1e6ef3488a2f138b27ee095
Matt McNaney <mcnaneym@appstate.edu>
2022-09-07 09:35:14 -0400

    Sorts methods alphabetically by access designation.

--- src/AbstractClass/AbstractView.php
+++ src/AbstractClass/AbstractView.php
@@ -21,6 +21,17 @@ class AbstractView
     const directory = PHPWS_SOURCE_DIR . 'mod/award/';
     const http = PHPWS_SOURCE_HTTP . 'mod/award/';
 
+    public static function centerCard(string $title, string $content)
+    {
+        return self::getTemplate('CenterCard', ['title' => $title, 'content' => $content]);
+    }
+
+    public static function errorPage()
+    {
+        $siteContactEmail = \phpws2\Settings::get('award', 'siteContactEmail');
+        return self::getTemplate('User/Error', ['contactEmail' => $siteContactEmail]);
+    }
+
     public static function getTemplate(string $templateFile, array $values = [], bool $css = false)
     {
         if ($css) {
@@ -35,70 +46,6 @@ class AbstractView
         $template = new Template($values);
         $template->setModuleTemplate('award', $templateFile . '.html');
         return $template->get();
-    }
-
-    protected static function getDirectory()
-    {
-        return self::directory;
-    }
-
-    protected static function getHttp()
-    {
-        return self::http;
-    }
-
-    private static function addScriptVars($vars)
-    {
-        if (empty($vars)) {
-            return null;
-        }
-        foreach ($vars as $key => $value) {
-            $varList[] = "const $key = " . json_encode($value, JSON_NUMERIC_CHECK) . ';';
-        }
-        return '<script type="text/javascript">' . implode('', $varList) . '</script>';
-    }
-
-    public static function errorPage()
-    {
-        $siteContactEmail = \phpws2\Settings::get('award', 'siteContactEmail');
-        return self::getTemplate('User/Error', ['contactEmail' => $siteContactEmail]);
-    }
-
-    public static function centerCard(string $title, string $content)
-    {
-        return self::getTemplate('CenterCard', ['title' => $title, 'content' => $content]);
-    }
-
-    protected static function getScript($scriptName)
-    {
-        $jsDirectory = self::getHttp() . 'javascript/';
-        if (AWARD_SYSTEM_SETTINGS['productionMode']) {
-            $path = $jsDirectory . 'build/' . self::getAssetPath($scriptName);
-        } else {
-            $path = "{$jsDirectory}dev/$scriptName.js";
-        }
-        $script = "<script type='text/javascript' src='$path'></script>";
-        return $script;
-    }
-
-    protected static function getAssetPath($scriptName)
-    {
-        if (!is_file(self::getDirectory() . 'assets.json')) {
-            exit('Missing assets.json file. Run "npm run build" in the award directory.');
-        }
-        $jsonRaw = file_get_contents(self::getDirectory() . 'assets.json');
-        $json = json_decode($jsonRaw, true);
-        if (!isset($json[$scriptName]['js'])) {
-            throw new \Exception('Script file not found among assets.');
-        }
-        return $json[$scriptName]['js'];
-    }
-
-    protected static function menu($active)
-    {
-        $auth = \Current_User::getAuthorization();
-        $params = ['active' => $active, 'logoutUrl' => $auth->logout_link];
-        return self::getTemplate('Admin/Menu', $params);
     }
 
     /**
@@ -126,6 +73,59 @@ class AbstractView
 <div id="$view_name"><p>Loading. Please wait.</p></div>
 EOF;
         return $content;
+    }
+
+    protected static function getAssetPath($scriptName)
+    {
+        if (!is_file(self::getDirectory() . 'assets.json')) {
+            exit('Missing assets.json file. Run "npm run build" in the award directory.');
+        }
+        $jsonRaw = file_get_contents(self::getDirectory() . 'assets.json');
+        $json = json_decode($jsonRaw, true);
+        if (!isset($json[$scriptName]['js'])) {
+            throw new \Exception('Script file not found among assets.');
+        }
+        return $json[$scriptName]['js'];
+    }
+
+    protected static function getDirectory()
+    {
+        return self::directory;
+    }
+
+    protected static function getHttp()
+    {
+        return self::http;
+    }
+
+    protected static function getScript($scriptName)
+    {
+        $jsDirectory = self::getHttp() . 'javascript/';
+        if (AWARD_SYSTEM_SETTINGS['productionMode']) {
+            $path = $jsDirectory . 'build/' . self::getAssetPath($scriptName);
+        } else {
+            $path = "{$jsDirectory}dev/$scriptName.js";
+        }
+        $script = "<script type='text/javascript' src='$path'></script>";
+        return $script;
+    }
+
+    protected static function menu($active)
+    {
+        $auth = \Current_User::getAuthorization();
+        $params = ['active' => $active, 'logoutUrl' => $auth->logout_link];
+        return self::getTemplate('Admin/Menu', $params);
+    }
+
+    private static function addScriptVars($vars)
+    {
+        if (empty($vars)) {
+            return null;
+        }
+        foreach ($vars as $key => $value) {
+            $varList[] = "const $key = " . json_encode($value, JSON_NUMERIC_CHECK) . ';';
+        }
+        return '<script type="text/javascript">' . implode('', $varList) . '</script>';
     }
 
 }
