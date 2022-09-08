54db233be36b7b605134c8e9433c7102ea65ef07
Matt McNaney <mcnaneym@appstate.edu>
2022-09-08 12:17:08 -0400

    Adds awardId, cycleId, and a timestamp property to object.

--- src/Resource/CycleLog.php
+++ src/Resource/CycleLog.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 /**
  * MIT License
@@ -11,30 +12,52 @@ declare(strict_types=1);
  */
 
 namespace award\Resource;
+
+use award\Resource\Cycle;
+
 /**
- * @table cyclelog 
+ * @table cyclelog
  */
 class CycleLog extends award\AbstractResource
 {
+
     /**
      * @var string
      */
     private string $action;
 
     /**
+     * The key to the award
      * @var int
      */
-    private int $documentId;
+    private int $awardId = 0;
+
+    /**
+     * The key to the cycle
+     * @var int
+     */
+    private int $cycleId = 0;
 
     /**
      * @var int
      */
-    private int $participantId;
+    private int $documentId = 0;
+
+    /**
+     * @var int
+     */
+    private int $participantId = 0;
+
+    /**
+     * Timestamp for log entry
+     * @var string
+     */
+    private string $stamped;
 
     /**
      * @return string
      */
-    public function getAction() : string
+    public function getAction(): string
     {
         return $this->action;
     }
@@ -42,7 +65,23 @@ class CycleLog extends award\AbstractResource
     /**
      * @return int
      */
-    public function getDocumentId() : int
+    public function getAwardId(): int
+    {
+        return $this->awardId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCycleId(): int
+    {
+        return $this->cycleId;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDocumentId(): int
     {
         return $this->documentId;
     }
@@ -50,24 +89,39 @@ class CycleLog extends award\AbstractResource
     /**
      * @return int
      */
-    public function getParticipantId() : int
+    public function getParticipantId(): int
     {
         return $this->participantId;
+    }
+
+    public function getStamped(): string
+    {
+        return $this->stamped;
     }
 
     /**
      * @param string $action
      */
-    public function setAction(string $action) : self
+    public function setAction(string $action): self
     {
         $this->action = $action;
         return $this;
     }
 
     /**
+     * Sets the award and cycle id with the $cycle parameter.
+     * @param Cycle $cycle
+     */
+    public function setCycle(Cycle $cycle)
+    {
+        $this->awardId = $cycle->awardId;
+        $this->cycleId = $cycle->id;
+    }
+
+    /**
      * @param int $documentId
      */
-    public function setDocumentId(int $documentId) : self
+    public function setDocumentId(int $documentId): self
     {
         $this->documentId = $documentId;
         return $this;
@@ -76,9 +130,15 @@ class CycleLog extends award\AbstractResource
     /**
      * @param int $participantId
      */
-    public function setParticipantId(int $participantId) : self
+    public function setParticipantId(int $participantId): self
     {
         $this->participantId = $participantId;
+        return $this;
+    }
+
+    public function setStamped(string $stamped)
+    {
+        $this->stamped = $stamped;
         return $this;
     }
 
