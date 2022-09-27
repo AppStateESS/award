f03386b853eda4eb1b7ae9c39c9694257f99965b
Matt McNaney <mcnaneym@appstate.edu>
2022-09-27 15:35:50 -0400

    Adds new factory for nominations.
    + Includes getCycleCount() and listing().

---
+++ src/Factory/NominationFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Factory;
+
+use award\AbstractClass\AbstractFactory;
+use award\Resource\Award;
+use award\Resource\Cycle;
+use award\Resource\Nomination;
+use phpws2\Database;
+use Canopy\Request;
+use award\Exception\ResourceNotFound;
+
+class NominationFactory extends AbstractFactory
+{
+
+    protected static string $table = 'award_nomination';
+    protected static string $resourceClassName = 'award\Resource\Nomination';
+
+    public static function getCycleCount(int $cycleId)
+    {
+        extract(self::getDBWithTable());
+        $table->addFieldConditional('cycleId', $cycleId);
+        $count = "count({$table->getField('id')})";
+        $expression = new Database\Expression($count, 'nominations');
+        $table->addField($expression);
+        return $db->selectColumn();
+    }
+
+    public static function listing(array $options = [])
+    {
+        extract(self::getDBWithTable());
+        return $db->select();
+    }
+
+}
