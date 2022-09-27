57f8fd30ecbe26a5df2d6054837e336d201fc3c5
Matt McNaney <mcnaneym@appstate.edu>
2022-09-27 15:35:13 -0400

    New controller for nomination admin functions.

---
+++ src/Controller/Admin/Nomination.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Controller\Admin;
+
+use award\AbstractClass\AbstractController;
+use award\Factory\NominationFactory;
+use Canopy\Request;
+
+class Nomination extends AbstractController
+{
+
+    protected function listJson(Request $request)
+    {
+        return NominationFactory::listing(['cycleId' => $request->pullGetInteger('cycleId')]);
+    }
+
+}
