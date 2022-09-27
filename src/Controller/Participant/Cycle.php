538f47cf5807120f4e40430182278092b8c1f521
Matt McNaney <mcnaneym@appstate.edu>
2022-09-27 15:35:26 -0400

    Adds Cycle controller for participants.

---
+++ src/Controller/Participant/Cycle.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Controller\Participant;
+
+use Canopy\Request;
+use award\AbstractClass\AbstractController;
+use award\Factory\CycleFactory;
+use award\Exception\ResourceNotFound;
+
+class Cycle extends AbstractController
+{
+
+}
