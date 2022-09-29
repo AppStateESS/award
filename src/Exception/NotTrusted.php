62045dfdde4682b136466d69d6ff04389fb65636
Matt McNaney <mcnaneym@appstate.edu>
2022-09-29 09:28:52 -0400

    Adds new exception for participant attempting a trusted action.

---
+++ src/Exception/NotTrusted.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Exception;
+
+class NotTrusted extends \Exception
+{
+
+    public function __construct(string $className, string $propertyType)
+    {
+        parent::__construct("participant attempted a trusted action");
+    }
+
+}
