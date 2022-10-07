2558923ab44d0608648050f93089fc0c5d46efe4
Matt McNaney <mcnaneym@appstate.edu>
2022-10-07 12:00:26 -0400

    Adds canComplete, create, and getByNominator functions.
    + canComplete - returns if a nomination ready to complete.
    + create - returns an empty nomination.
    + getByNominatorId - returns a nomination (if found) for
      by the nominator's participant resource id.
    + Fixes the AwardFactory::build call with the proper id.
    + Adds cycleId to listing call to prevent judge inclusion.

--- src/Factory/NominationFactory.php
+++ src/Factory/NominationFactory.php
@@ -27,6 +27,37 @@ class NominationFactory extends AbstractFactory
     protected static string $table = 'award_nomination';
     protected static string $resourceClassName = 'award\Resource\Nomination';
 
+    public static function canComplete(Award $award, Nomination $nomination)
+    {
+        return (!$award->nominationReasonRequired || ($award->nominationReasonRequired && $nomination->reasonComplete)) &&
+            (!$award->referencesRequired || ($award->referencesRequired && $nomination->referencesSelected));
+    }
+
+    public static function create(int $nominatorId, int $participantId, int $awardId, int $cycleId)
+    {
+        $nomination = self::build();
+        $nomination->setAwardId($awardId)->
+            setCycleId($cycleId)->
+            setParticipantId($participantId)->
+            setNominatorId($nominatorId);
+        self::save($nomination);
+        return $nomination;
+    }
+
+    public static function getByNominator(int $nominatorId, int $cycleId)
+    {
+        extract(self::getDBWithTable());
+        $table->addFieldConditional('nominatorId', $nominatorId);
+        $table->addFieldConditional('cycleId', $cycleId);
+        $row = $db->selectOneRow();
+        if ($row == false) {
+            return false;
+        }
+        $nomination = NominationFactory::build();
+        $nomination->setValues($row);
+        return $nomination;
+    }
+
     public static function getCycleCount(int $cycleId)
     {
         extract(self::getDBWithTable());
@@ -43,19 +74,30 @@ class NominationFactory extends AbstractFactory
         return $db->select();
     }
 
-    public static function nominateParticipantText(int $participantId, int $cycleId, $reasonText)
+    /**
+     * TODO Rename this
+     * @param int $participantId
+     * @param int $cycleId
+     * @param string $reasonText
+     * @throws \Exception
+     */
+    public static function nominateParticipantText(int $participantId, int $cycleId, string $reasonText)
     {
+        if (empty($reasonText)) {
+            throw new \Exception('missing reason text');
+        }
         $nomination = self::build();
         $participant = ParticipantFactory::build($participantId);
         $cycle = CycleFactory::build($cycleId);
-        $award = AwardFactory::build($cycle->id);
+        $award = AwardFactory::build($cycle->awardId);
 
         ParticipantFactory::canBeNominated($participant, $cycle, $award);
 
         $nomination->setParticipantId($participant->id)->
             setAwardId($award->id)->
             setCycleId($cycle->id)->
-            setApproved(!$award->approvalRequired);
+            setReasonText($reasonText)->
+            setReasonComplete(true);
 
         self::save($nomination);
     }
