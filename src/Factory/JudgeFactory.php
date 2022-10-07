16024dc1f2eb2f16e8a39f6d814d2d0f8f2f4828
Matt McNaney <mcnaneym@appstate.edu>
2022-10-07 11:51:30 -0400

    Adds participantIdOnly option to listing.

--- src/Factory/JudgeFactory.php
+++ src/Factory/JudgeFactory.php
@@ -52,6 +52,16 @@ class JudgeFactory extends AbstractFactory
         return (bool) $db->selectOneRow();
     }
 
+    /**
+     * Options
+     * - cycleId (integer) Return only judges associated with this cycle.
+     * - particpantIdOnly (boolean) Return only judge ids.
+     * - includeParticipant (boolean) Add participant's email plus first and last name.
+     *
+     *
+     * @param array $options
+     * @return type
+     */
     public static function listing(array $options = [])
     {
         extract(self::getDBWithTable());
@@ -59,7 +69,13 @@ class JudgeFactory extends AbstractFactory
             $table->addFieldConditional('cycleId', $options['cycleId']);
         }
 
-        if (!empty($options['includeParticipant'])) {
+        if (!empty($options['participantIdOnly'])) {
+            $table->addField('participantId');
+            while ($row = $db->selectColumn()) {
+                $ids[] = $row;
+            }
+            return $ids;
+        } elseif (!empty($options['includeParticipant'])) {
             $partTable = $db->addTable('award_participant');
             $partTable->addField('firstName');
             $partTable->addField('lastName');
