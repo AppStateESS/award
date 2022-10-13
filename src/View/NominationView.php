0e10ff6ec4724c6227eb13e221add4d2271ca72f
Matt McNaney <mcnaneym@appstate.edu>
2022-10-13 16:11:36 -0400

    Adds cannotNominateParticipant view.
    + Adds errorByException: matches exception type with proper error
      screen.
    + Adds nominationStatus.
    + Adds reasonForm.

--- src/View/NominationView.php
+++ src/View/NominationView.php
@@ -21,39 +21,126 @@ use award\Factory\SettingFactory;
 use award\Factory\ParticipantFactory;
 use award\Resource\Cycle;
 use award\Resource\Award;
+use award\Resource\Nomination;
 use award\Resource\Participant;
 
 class NominationView extends AbstractView
 {
 
-    public static function deadlinePassed(Award $award, Cycle $cycle)
+    public static function cannotNominateParticipant()
     {
-        return 'The deadline for this cycle has passed. No more nominations are accepted';
+        return self::getTemplate('Error/CannotNominateParticipant');
+    }
+
+    public static function deadlinePassed(string $awardTitle)
+    {
+        return self::getTemplate('Error/CycleDeadline', ['awardTitle' => $awardTitle]);
+    }
+
+    /**
+     * Returns an error page view determined by the exception type.
+     * @param \Exception $exception
+     */
+    public static function errorByException(\Exception $exception, $award, $cycle)
+    {
+        $awardTitle = CycleView::getFullAwardTitle($award, $cycle);
+        switch (get_class($exception)) {
+            case 'award\Exception\CycleComplete':
+                $title = 'Cycle complete';
+                $content = CycleView::complete($awardTitle);
+                break;
+
+            case 'award\Exception\CycleEndDatePassed':
+                $title = 'Cycle deadline has passed';
+                $content = NominationView::deadlinePassed($awardTitle);
+                break;
+
+            case 'award\Exception\CannotNominateJudge':
+                $title = 'Nomination not allowed';
+                $content = NominationView::noJudges($awardTitle);
+                break;
+
+            case 'award\Exception\ParticipantPrivilegeMissing':
+                $title = 'Permission denied';
+                $content = NominationView::notPermitted($awardTitle);
+                break;
+
+            case 'award\Exception\NotTrusted':
+                $title = 'Nomination not allowed';
+                $content = NominationView::onlyTrusted();
+                break;
+
+            case 'award\Exception\ParticipantAlreadyNominated':
+            case 'award\Exception\InactiveParticipant':
+            case 'award\Exception\BannedParticipant':
+                $title = 'Cannot nominate this participant';
+                $content = NominationView::cannotNominateParticipant();
+
+                break;
+
+            default:
+                $title = 'Unknown error';
+                $content = 'Error: ' . $exception->getMessage();
+                break;
+        }
+
+        return self::centerCard($title, $content, 'danger');
     }
 
     /**
      * Error screen for judges trying to nominate.
      * @return string
      */
-    public static function noJudges($award, $cycle)
+    public static function noJudges(string $awardTitle)
     {
-        $title = self::getFullAwardTitle($award, $cycle);
-        return self::participantMenu('nomination') . self::centerCard("Nomination not allowed", self::getTemplate('Participant/NoJudges', ['awardTitle' => $title]), 'danger');
+        return self::getTemplate('Participant/NoJudges', ['awardTitle' => $awardTitle]);
     }
 
+    /**
+     * View that allows the participant choose who they want to nominate.
+     * @param Award $award
+     * @param Cycle $cycle
+     * @return string
+     */
     public static function nominate(Award $award, Cycle $cycle)
     {
-        $menu = self::participantMenu('nomination');
-        $content = self::scriptView('Nominate', ['cycle' => $cycle->getValues(), 'award' => $award->getParticipantValues()]);
-        return $menu . $content;
+        return self::scriptView('Nominate', ['cycle' => $cycle->getValues(), 'award' => $award->getParticipantValues()]);
     }
 
-    public static function nominateParticipant(Participant $participant, Award $award, Cycle $cycle)
+    /**
+     * Error page for mismatched nominator id
+     * @param string $awardTitle
+     */
+    public static function notPermitted(string $awardTitle)
     {
-        $menu = self::participantMenu('nomination');
-        $maxsize = \award\Factory\DocumentFactory::maximumUploadSize();
-        $content = self::scriptView('NominationCompletion', ['maxsize' => $maxsize, 'award' => $award->getParticipantValues(), 'cycle' => $cycle->getParticipantValues(), 'participant' => $participant->getParticipantValues()]);
-        return $menu . $content;
+        return self::getTemplate('Error/NominatorNotPermitted');
+    }
+
+    /**
+     * Determines the proper view for the nomination process based on its current status.
+     * @param Participant $nominator The person nominating someone for an award
+     * @param Nomination $nomination The current nomination object.
+     * @return string
+     */
+    public static function nominationStatus(Participant $nominator, Nomination $nomination)
+    {
+        $participant = ParticipantFactory::build($nomination->participantId);
+        $award = AwardFactory::build($nomination->awardId);
+        $cycle = CycleFactory::build($nomination->cycleId);
+        $tpl['approvalRequired'] = (bool) $award->approvalRequired;
+        $tpl['reasonRequired'] = (bool) $award->nominationReasonRequired;
+        $tpl['reasonComplete'] = (bool) $nomination->reasonComplete;
+        $tpl['referencesRequired'] = $award->referencesRequired;
+        $tpl['referencesSelected'] = (bool) $nomination->referencesSelected;
+        $tpl['nominationComplete'] = (bool) $nomination->completed;
+        $tpl['firstName'] = $participant->firstName;
+        $tpl['lastName'] = $participant->lastName;
+        $tpl['awardTitle'] = self::getFullAwardTitle($award, $cycle);
+        $tpl['nominationId'] = $nomination->id;
+
+        $tpl['canComplete'] = NominationFactory::canComplete($award, $nomination);
+
+        return self::getTemplate('Participant/NominationStatus', $tpl);
     }
 
     /**
@@ -64,6 +151,20 @@ class NominationView extends AbstractView
     {
         $menu = self::participantMenu('nominations');
         return $menu . '<p>List of all people participant has nominated.</p>';
+    }
+
+    public static function reasonForm(Award $award, Cycle $cycle, Nomination $nomination, Participant $nominee)
+    {
+        $maxsize = \award\Factory\DocumentFactory::maximumUploadSize();
+
+        $js = [
+            'maxsize' => $maxsize,
+            'award' => $award->getParticipantValues(),
+            'cycle' => $cycle->getParticipantValues(),
+            'nomination' => $nomination->getValues(),
+            'participant' => $nominee->getParticipantValues()
+        ];
+        return self::scriptView('NominationReason', $js);
     }
 
     /**
