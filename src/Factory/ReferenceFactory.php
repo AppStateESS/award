6fba5732ca8cfde2cfa59702e73b3f252561f7db
Matt McNaney <mcnaneym@appstate.edu>
2022-10-26 15:18:49 -0400

    Adds listing().

--- src/Factory/ReferenceFactory.php
+++ src/Factory/ReferenceFactory.php
@@ -20,5 +20,47 @@ use phpws2\Database;
 
 class ReferenceFactory extends AbstractFactory
 {
-//put your code here
+
+    protected static string $resourceClassName = 'award\Resource\Reference';
+    protected static string $table = 'award_reference';
+
+    /**
+     * Options
+     * - cycleId (integer) Return only references associated with this cycle.
+     * - nominationId (integer) Return only references associated with this nomination.
+     * - participantIdOnly (boolean) Return only participant ids.
+     * - includeParticipant (boolean) Add participant's email plus first and last name.
+     *
+     *
+     * @param array $options
+     * @return type
+     */
+    public static function listing(array $options = [])
+    {
+        extract(self::getDBWithTable());
+        if (!empty($options['cycleId'])) {
+            $table->addFieldConditional('cycleId', $options['cycleId']);
+        }
+
+        if (!empty($options['nominationId'])) {
+            $table->addFieldConditional('nominationId', $options['nominationId']);
+        }
+
+        if (!empty($options['participantIdOnly'])) {
+            $ids = [];
+            $table->addField('participantId');
+            while ($row = $db->selectColumn()) {
+                $ids[] = $row;
+            }
+            return $ids;
+        } elseif (!empty($options['includeParticipant'])) {
+            $partTable = $db->addTable('award_participant');
+            $partTable->addField('firstName');
+            $partTable->addField('lastName');
+            $partTable->addField('email');
+            $db->joinResources($table, $partTable, new Database\Conditional($db, $table->getField('participantId'), $partTable->getField('id'), '='));
+        }
+        return $db->select();
+    }
+
 }
