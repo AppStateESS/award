95eb1e73015f19ba83c68c8150a9000a2467ea10
Matt McNaney <mcnaneym@appstate.edu>
2022-11-08 11:11:02 -0500

    Adds nominationReferencesComplete().
    + Adds count option to listing.

--- src/Factory/ReferenceFactory.php
+++ src/Factory/ReferenceFactory.php
@@ -16,6 +16,7 @@ namespace award\Factory;
 use award\Resource\Reference;
 use award\AbstractClass\AbstractFactory;
 use award\View\EmailView;
+use award\Resource\Nomination;
 use phpws2\Database;
 
 class ReferenceFactory extends AbstractFactory
@@ -46,7 +47,7 @@ class ReferenceFactory extends AbstractFactory
      * - nominationId (integer) Return only references associated with this nomination.
      * - participantIdOnly (boolean) Return only participant ids.
      * - includeParticipant (boolean) Add participant's email plus first and last name.
-     *
+     * - count (boolean) Returns the number of references.
      *
      * @param array $options
      * @return type
@@ -69,6 +70,11 @@ class ReferenceFactory extends AbstractFactory
                 $ids[] = $row;
             }
             return $ids;
+        } elseif (!empty($options['count'])) {
+            $idField = $table->getField('id');
+            $countExpression = new Database\Expression("count($idField)");
+            $table->addField($countExpression);
+            return $db->selectColumn();
         } elseif (!empty($options['includeParticipant'])) {
             $partTable = $db->addTable('award_participant');
             $partTable->addField('firstName');
@@ -77,6 +83,48 @@ class ReferenceFactory extends AbstractFactory
             $db->joinResources($table, $partTable, new Database\Conditional($db, $table->getField('participantId'), $partTable->getField('id'), '='));
         }
         return $db->select();
+    }
+
+    /**
+     * Returns reference status.
+     * Returns TRUE if
+     * - The award does not require references, or
+     * - The amount of references meets or exceed the award's requirement AND
+     *   - the award's reference reason is required AND
+     *     each reference as reason text or an associated document.
+     *     OR
+     *   - the award's reference reason is not required
+     * @param Nomination $nomination
+     * @return boolean
+     */
+    public static function nominationReferencesComplete(Nomination $nomination)
+    {
+        $award = AwardFactory::build($nomination->awardId);
+        $references = self::listing(['nominationId' => $nomination->id]);
+        if (!$award->referencesRequired) {
+            // references are not required, return true
+            return true;
+        } elseif (count($references) < $award->referencesRequired) {
+            // reference count is under the requirement, return false
+            return false;
+        }
+
+        // Now test reference reasons
+        if (!$award->referenceReasonRequired) {
+            // reasons are not required, return true
+            return true;
+        } else {
+            // Reasons are required, now check each reference to see
+            // if their reason is set
+            foreach ($references as $ref) {
+                if (empty($ref['reasonText']) && empty($ref['reasonDocument'])) {
+                    // both reasonText and reasonDocument are empty, return false
+                    return false;
+                }
+            }
+        }
+        // references passed the reason check, return true
+        return true;
     }
 
 }
