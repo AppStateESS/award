68670ecf55acdc3bb288e6a23f9c2067a5dfe7e1
Matt McNaney <mcnaneym@appstate.edu>
2022-11-18 11:30:40 -0500

    Updates the reference interface for the participant.

--- javascript/Participant/ChooseReference/Current.tsx
+++ javascript/Participant/ChooseReference/Current.tsx
@@ -1,22 +1,77 @@
 'use strict'
 import React from 'react'
 import PropTypes from 'prop-types'
-import {ParticipantResource} from '../../ResourceTypes'
+import {ReferenceResource} from '../../ResourceTypes'
 
-type Props = {referenceList: ParticipantResource[]}
+import ReasonStatus from './ReasonStatus'
+import RemindIcon from './RemindIcon'
+import {reasonCompleted} from '../../Share/Reference'
+import {sendReferenceReasonReminder} from '../../Share/ReferenceXHR'
 
-const Current = ({referenceList}: Props) => {
-  const content = referenceList.map((value) => {
-    return (
-      <div key={`reference-${value.id}`}>
-        <a href={`mailto:${value.email}`}>
-          {value.firstName} {value.lastName}
-        </a>
-      </div>
-    )
-  })
-  return <div>{content}</div>
+interface CurrentProps {
+  referenceList: ReferenceResource[]
+  referenceReasonRequired: boolean
+  reminderGrace: number
 }
 
-Current.propTypes = {referenceList: PropTypes.array}
+const sendReminder = (referenceId: number) => {
+  sendReferenceReasonReminder(referenceId).then((response) => {
+    console.log(response.data)
+  })
+}
+
+const Current = ({
+  referenceList,
+  referenceReasonRequired,
+  reminderGrace,
+}: CurrentProps) => {
+  const content = referenceList.map((value) => {
+    return (
+      <tr key={`reference-${value.id}`}>
+        <td>
+          {value.firstName} {value.lastName}
+        </td>
+        <td>
+          <a href={`mailto:${value.email}`}>{value.email}</a>
+        </td>
+        <td>
+          <ReasonStatus
+            reference={value}
+            reasonRequired={referenceReasonRequired}
+          />
+        </td>
+        <td>
+          {reasonCompleted(value) ? (
+            <span />
+          ) : (
+            <RemindIcon
+              reference={value}
+              reminderGrace={reminderGrace}
+              sendReminder={() => sendReminder(value.id)}
+            />
+          )}
+        </td>
+      </tr>
+    )
+  })
+  return (
+    <table className="table table-striped">
+      <tbody>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Reason complete</th>
+          <th>Send Reminder</th>
+        </tr>
+        {content}
+      </tbody>
+    </table>
+  )
+}
+
+Current.propTypes = {
+  referenceList: PropTypes.array,
+  referenceReasonRequired: PropTypes.bool,
+  reminderGrace: PropTypes.number,
+}
 export default Current
