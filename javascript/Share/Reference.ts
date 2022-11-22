4295aacf8fc637704776251fb99efc2758690e31
Matt McNaney <mcnaneym@appstate.edu>
2022-11-22 16:04:00 -0500

    Rewrites reasonCompleted function.

--- javascript/Share/Reference.ts
+++ javascript/Share/Reference.ts
@@ -1,10 +1,7 @@
 import {ReferenceResource} from '../ResourceTypes'
 
 const reasonCompleted = (reference: ReferenceResource) => {
-  return (
-    (reference.reasonDocument !== null && reference.reasonDocument > 0) ||
-    (reference.reasonText !== null && reference.reasonText.length > 0)
-  )
+  return reference.reasonDocument > 0 || reference.reasonText?.length > 0
 }
 
 export {reasonCompleted}
