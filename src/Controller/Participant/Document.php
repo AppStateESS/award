59509e0772da913ce24aac24d3800bcc596418a0
Matt McNaney <mcnaneym@appstate.edu>
2022-11-23 07:24:18 -0500

    Adds Document controller file.

---
+++ src/Controller/Participant/Document.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Controller\Participant;
+
+use Canopy\Request;
+use award\Factory\DocumentFactory;
+
+class Document
+{
+
+}
