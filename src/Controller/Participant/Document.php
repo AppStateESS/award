64fd39e22ff0300bbbb2d1e00fa913639446cf0a
Matt McNaney <mcnaneym@appstate.edu>
2022-12-07 09:21:24 -0500

    Adds delete and downloadHtml control functions.

--- src/Controller/Participant/Document.php
+++ src/Controller/Participant/Document.php
@@ -14,9 +14,38 @@ declare(strict_types=1);
 namespace award\Controller\Participant;
 
 use Canopy\Request;
+use award\AbstractClass\AbstractController;
 use award\Factory\DocumentFactory;
+use award\Exception\MissingDocument;
+use award\Exception\ParticipantPrivilegeMissing;
+use award\Factory\DocumentFactory;
+use award\Factory\ParticipantFactory;
 
-class Document
+class Document extends AbstractController
 {
+
+    protected function delete()
+    {
+        $document = DocumentFactory::build($this->id);
+        if (!ParticipantFactory::currentOwnsDocument($document)) {
+            throw new ParticipantPrivilegeMissing();
+        }
+        DocumentFactory::delete($document);
+
+        return ['success' => true];
+    }
+
+    protected function downloadHtml()
+    {
+        $document = DocumentFactory::build($this->id);
+        if ($document === false) {
+            throw new MissingDocument();
+        }
+        if (ParticipantFactory::currentOwnsDocument($document)) {
+            DocumentFactory::download($document);
+        } else {
+            throw new ParticipantPrivilegeMissing();
+        }
+    }
 
 }
