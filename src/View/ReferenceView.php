08161ef1651814940f504616dfa0124c7dcf5496
Matt McNaney <mcnaneym@appstate.edu>
2022-12-07 09:37:21 -0500

    Adds passedUpdate error view.
    + Adds reasonForm view.
    + Adds referenceReasonNotRequired error view.

--- src/View/ReferenceView.php
+++ src/View/ReferenceView.php
@@ -14,13 +14,43 @@ declare(strict_types=1);
 namespace award\View;
 
 use award\AbstractClass\AbstractView;
+use award\Factory\ReferenceFactory;
+use award\Factory\DocumentFactory;
+use award\Resource\Reference;
+use award\Resource\Participant;
 
 class ReferenceView extends AbstractView
 {
 
+    public static function passedUpdate()
+    {
+        return self::centerCard('Award deadline has passed', self::getTemplate('Error/ReferencePassedUpdate'), 'danger');
+    }
+
+    public static function reasonForm(Reference $reference)
+    {
+        $values = ReferenceFactory::getAssociated($reference, ['nomination'], true);
+        if (!$values['award']['referenceReasonRequired']) {
+            return self::referenceReasonNotRequired();
+        }
+        $values['reference'] = $reference->getValues();
+        $values['maxsize'] = \award\Factory\DocumentFactory::maximumUploadSize();
+        $document = DocumentFactory::build();
+
+        $reasonDocument = $reference->getReasonDocument();
+        $values['currentReasonDocument'] = DocumentFactory::build($reasonDocument)->getValues();
+
+        return self::scriptView('ReferenceReasonForm', $values);
+    }
+
     public static function referencesNotRequired()
     {
         return self::centerCard('References not required', self::getTemplate('Error/ReferencesNotRequired'), 'danger');
+    }
+
+    public static function referenceReasonNotRequired()
+    {
+        return self::centerCard('Reference endorsement not required', self::getTemplate('Error/ReferenceReasonNotRequired'), 'danger');
     }
 
 }
