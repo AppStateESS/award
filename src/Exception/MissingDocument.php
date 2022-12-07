97c08a5c53987cffe3b2f3dc08a369520c49a638
Matt McNaney <mcnaneym@appstate.edu>
2022-12-07 09:41:30 -0500

    Adds new exception for documents that cannot be found.

---
+++ src/Exception/MissingDocument.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * MIT License
+ * Copyright (c) 2022 Electronic Student Services @ Appalachian State University
+ *
+ * See LICENSE file in root directory for copyright and distribution permissions.
+ *
+ * @author Matthew McNaney <mcnaneym@appstate.edu>
+ * @license https://opensource.org/licenses/MIT
+ */
+
+namespace award\Exception;
+
+class MissingDocument extends \Exception
+{
+
+    public function __construct()
+    {
+        parent::__construct("document not found");
+    }
+
+}
