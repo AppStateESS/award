a4c597fccf35479ca17691c0683bc49f64321f7a
Matt McNaney <mcnaneym@appstate.edu>
2022-12-07 09:32:10 -0500

    Adds referenceId to resource.

--- src/Resource/Document.php
+++ src/Resource/Document.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 /**
  * MIT License
@@ -11,30 +12,57 @@ declare(strict_types=1);
  */
 
 namespace award\Resource;
+
+use award\AbstractClass\AbstractResource;
+
 /**
- * @table document 
+ * @table document
  */
-class Document extends award\AbstractResource
+class Document extends AbstractResource
 {
+
+    /**
+     * Date created
+     * @var \DateTime
+     */
+    protected \DateTime $created;
+
     /**
      * @var string
      */
     private string $filename;
 
     /**
+     * Id for the nomination reason document. Only > 0 for nominations.
      * @var int
      */
-    private int $nominationId;
+    private int $nominationId = 0;
+
+    /**
+     * Id for the reference reason document. Only > 0 for references.
+     * @var int
+     */
+    private int $referenceId = 0;
 
     /**
      * @var string
      */
     private string $title;
 
+    public function __construct()
+    {
+        parent::__construct('award_document');
+    }
+
+    public function getCreated(string $format = null)
+    {
+        return $this->created->format($format ?? 'Y-m-d H:i:s');
+    }
+
     /**
      * @return string
      */
-    public function getFilename() : string
+    public function getFilename(): string
     {
         return $this->filename;
     }
@@ -42,23 +70,33 @@ class Document extends award\AbstractResource
     /**
      * @return int
      */
-    public function getNominationId() : int
+    public function getNominationId(): int
     {
         return $this->nominationId;
+    }
+
+    public function getReferenceId(): int
+    {
+        return $this->referenceId;
     }
 
     /**
      * @return string
      */
-    public function getTitle() : string
+    public function getTitle(): string
     {
         return $this->title;
+    }
+
+    public function setCreated(string $datetime)
+    {
+        $this->created = new \DateTime($datetime);
     }
 
     /**
      * @param string $filename
      */
-    public function setFilename(string $filename) : self
+    public function setFilename(string $filename): self
     {
         $this->filename = $filename;
         return $this;
@@ -67,19 +105,30 @@ class Document extends award\AbstractResource
     /**
      * @param int $nominationId
      */
-    public function setNominationId(int $nominationId) : self
+    public function setNominationId(int $nominationId): self
     {
         $this->nominationId = $nominationId;
+        return $this;
+    }
+
+    public function setReferenceId(int $referenceId): self
+    {
+        $this->referenceId = $referenceId;
         return $this;
     }
 
     /**
      * @param string $title
      */
-    public function setTitle(string $title) : self
+    public function setTitle(string $title): self
     {
         $this->title = $title;
         return $this;
+    }
+
+    public function stampCreated()
+    {
+        $this->created = new \DateTime();
     }
 
 }
