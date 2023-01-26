efa55709f92211e5a0447a20817597a0a30d4f74
Matt McNaney <mcnaneym@appstate.edu>
2023-01-26 10:50:43 -0500

    Adds missing list.

--- javascript/Admin/InvitationList/ParticipantInvites.tsx
+++ javascript/Admin/InvitationList/ParticipantInvites.tsx
@@ -3,7 +3,28 @@ import React from 'react'
 import {InvitationResource} from '../../ResourceTypes'
 
 const ParticipantInvites = ({listing}: {listing: InvitationResource[]}) => {
-  return <div>ParticipantInvites is ready for content.</div>
+  return (
+    <table className="table table-striped">
+      <tbody>
+        <tr>
+          <th>Email</th>
+          <th>Date sent</th>
+          <th>Date responded</th>
+        </tr>
+        {listing.map((value) => {
+          return (
+            <tr key={`invite-${value.id}`}>
+              <td>
+                <a href={`mailto:${value.email}`}>{value.email}</a>
+              </td>
+              <td>{value.created}</td>
+              <td>{value.updated}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
 }
 
 export default ParticipantInvites
